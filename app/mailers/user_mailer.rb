4fa8a52394946398936ee12c9298181db31e28ba
Jiayi Zheng <thebluber@gmail.com>
2014-12-09 17:43:48 +0100

    Update password_resets_controller: Add error flash msg in #create action

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,8 +4,8 @@ class UserMailer < ActionMailer::Base
 
   def reset_password_email(user)
     @user = user
-    @url  = "http://0.0.0.0:3000/password_resets/#{user.reset_password_token}/edit"
-    mail(:to => user.email,
+    @url  = "http://0.0.0.0:3000/password_resets/#{@user.reset_password_token}/edit"
+    mail(:to => @user.email,
          :subject => "Your password reset request")
   end
 end
