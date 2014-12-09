01ba7c72ef4d9a98978b645a4df2e32605652dd8
Jiayi Zheng <thebluber@gmail.com>
2014-12-09 17:37:32 +0100

    Add flash_message helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_message
+    flash.map do |key, msg|
+      if key == "alert" || key == "error"
+        content_tag :div, msg, :class => 'alert alert-danger'
+      else
+        content_tag :div, msg, :class => 'alert'
+      end
+    end.join.html_safe
+  end
 end
