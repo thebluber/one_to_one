629cd357dc75b74e45982252e0c0605f479ba16d
Jiayi Zheng <thebluber@gmail.com>
2014-12-15 16:14:49 +0100

    Add students method in course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,8 @@ class Course < ActiveRecord::Base
 
   validates_presence_of :title, allow_blank: false
   validates_inclusion_of :active, in: [true, false]
+
+  def students
+    self.course_buckets.map(&:student).uniq
+  end
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -12,4 +12,19 @@ RSpec.describe Course, :type => :model do
       expect(course.save).to eql false
     end
   end
+
+  describe "students" do
+    let(:user1){ create(:user) }
+    let(:user2){ create(:user) }
+    let(:course){ create(:active_course) }
+    before do
+      user1.student.course_buckets.create
+      user1.student.course_buckets.map{|bucket| bucket.courses << course }
+      user2.student.course_buckets.map{|bucket| bucket.courses << course }
+      course.reload
+    end
+    it "should return all students assigned to course" do
+      expect(course.students).to eql([user1.student, user2.student])
+    end
+  end
 end
