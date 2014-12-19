6c331a3ab8a254d8f258888cd6a957596314d4fc
Jiayi Zheng <thebluber@gmail.com>
2014-12-20 00:44:54 +0100

    Add current_semester to semester model

--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,4 +1,8 @@
 class Semester < ActiveRecord::Base
   has_many :course_buckets
   validates :start, presence: true
+
+  def self.current_semester
+    Semester.where("start <= ?", DateTime.now).last
+  end
 end
--- spec/models/semester_spec.rb
+++ spec/models/semester_spec.rb
@@ -3,4 +3,16 @@ require 'rails_helper'
 RSpec.describe Semester, :type => :model do
   it{ should validate_presence_of(:start) }
   it{ should have_many(:course_buckets) }
+  it "should return the current_semester" do
+    old = create(:semester, start: '2014-4-10')
+    current = create(:semester, start: '2014-10-10')
+    future = create(:semester, start: '2015-4-10')
+    t = Time.local(2014, 12, 1, 10, 5, 0)
+    Timecop.travel(t)
+    expect(Semester.current_semester).to eql current
+    t = Time.local(2015, 5, 1, 10, 5, 0)
+    Timecop.travel(t)
+    expect(Semester.current_semester).to eql future
+    Timecop.return
+  end
 end
